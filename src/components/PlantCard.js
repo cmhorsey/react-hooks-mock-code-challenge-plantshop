0511ada67a26ddacbe924c8282b6acc76fbe7864
Cayla Horsey <cayla.horsey@powerhrg.com>
2024-06-21 09:36:02 -0400

    Complete sold out button

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,15 +1,25 @@
-import React from "react"
+import React, { useState } from "react"
 
 function PlantCard({ name, image, price }) {
+  const [soldOut, setSoldOut] = useState(true)
+
+  function handleSoldOutClick() {
+    if (!soldOut) {
+      setSoldOut(true)
+    } else setSoldOut(false)
+  }
+
   return (
     <li className="card">
       <img src={image} alt={name} />
       <h4>{name}</h4>
       <p>Price: {price}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
+      {soldOut ? (
+        <button onClick={handleSoldOutClick} className="primary">
+          In Stock
+        </button>
       ) : (
-        <button>Out of Stock</button>
+        <button onClick={handleSoldOutClick}>Out of Stock</button>
       )}
     </li>
   )
